4a00cd78c7052cd6d396e844c55868b41d5424db
iamnotmarcel <marcel.miljak@tsn.at>
2021-05-26 10:19:46 +0200

    commi

--- DJANGO/Project_FSST/GithubReview/views.py
+++ DJANGO/Project_FSST/GithubReview/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, HttpResponse
 import json
 import requests
-import base64
-from github import Github
-from pprint import pprint
 
 # Create your views here.
 def frontpage(request):
