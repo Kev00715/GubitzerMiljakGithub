af904e79b2244dc5cc740975e7b0601dd31d2e38
iamnotmarcel <marcel.miljak@tsn.at>
2021-06-17 21:17:51 +0200

    commit

--- DJANGO/Project_FSST/GithubReview/views.py
+++ DJANGO/Project_FSST/GithubReview/views.py
@@ -3,28 +3,13 @@ from django.shortcuts import render, HttpResponse
 import json
 import requests
 import base64
+import os.path
 from github import Github
 from pprint import pprint
 import json
 
-# Github username
-username = "Kev00715"
-username2 = "OE7DIO"
-username3 = "xTzarol"
-username4 = "edinkhauser"
-username5 = "edinkhauser"
-username6 = "Manuel-Faehndrich"
-
 # pygithub object
-g = Github("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")
-
-# get that user by username
-user = g.get_user(username)
-user2 = g.get_user(username2)
-user3 = g.get_user(username3)
-user4 = g.get_user(username4)
-user5 = g.get_user(username5)
-user6 = g.get_user(username6)
+g = Github()
 
 def data_user(user):
     return f'-----Project Information for {user}-----'
@@ -34,9 +19,6 @@ def get_repos(user):
     for repo in user.get_repos():
         # repository full name
         output = "\n".join((output, f"Full name: {repo.full_name}"))
-        
-        # repository owner
-        output = "\n".join((output, f"Owner: {repo.owner.name}"))
 
         # repository description
         output = "\n".join((output, f"Description: {repo.description}"))
@@ -49,6 +31,12 @@ def get_repos(user):
 
         # programming language
         output = "\n".join((output, f"Language: {repo.language}"))
+        
+        # number of forks
+        #output = "\n".join((output, f"Number of forks: {repo.forks}"))
+        
+        # number of stars
+        #output = "\n".join((output, f"Number of stars: {repo.stargazers_count}"))
 
         output_lf = ["\n", output]
         #minuses = ["-" for i in range(50)]
@@ -59,17 +47,35 @@ def get_repos(user):
         for content in repo.get_contents(""):
             output = "\n".join((output, str(content)))
         
-        try:
-            # repo license
-            output = "\n".join((output, f"License: {base64.b64decode(repo.get_license().content.encode()).decode()}"))
-        except Exception:
-            pass
+        # try:
+        #     # repo license
+        #     output = "\n".join((output, f"License: {base64.b64decode(repo.get_license().content.encode()).decode()}"))
+        # except Exception:
+        #     pass
         
         return output
 
+def projectlist_htmlwrapper(urls, users):
+    output = ""
+
+    for url, user in zip(urls, users):
+        formatted_link = f"<a href=\"{url}\" target=\"_blank\">{user}</a>"
+        output = "\n".join((output, formatted_link))
+    
+    return output
+
+def repoview_htmwrapper(users):
+    output = ""
+
+    for user in users:
+        github_user = g.get_user(user)
+        output = "\n".join((output, data_user(user), get_repos(github_user)))
+    
+    return output
+
 # Create your views here.
 def frontpage(request):
-    return render(request, 'GithubReview/base.html')
+    return home(request)
 
 def index(request):
     project = requests.get('https://api.github.com/repos/Kev00715/GubitzerMiljakGithub')
@@ -77,24 +83,30 @@ def index(request):
     return HttpResponse(content)
 
 def home(request):
-    return render(request, 'GithubReview/base.html')
+    with open(os.path.join(os.path.dirname(__file__), "links.json"), "r") as file:
+        json_links = json.load(file)[0]
+    
+    links = json_links["links"]
+    users = json_links["user"]
+
+    menu = projectlist_htmlwrapper(links, users)
+
+    return render(request, 'GithubReview/base.html', {"links" : menu})
 
 def description(request):
-    var = get_repos(user)
-    var2 = get_repos(user2)
-    var3 = get_repos(user3)
-    var4 = get_repos(user4)
-    var5 = get_repos(user5)
-    var6 = get_repos(user6)
-    name_project = data_user(username)
-    name_project2 = data_user(username2)
-    name_project3 = data_user(username3)
-    name_project4 = data_user(username4)
-    name_project5 = data_user(username5)
-    name_project6 = data_user(username6)
+    with open(os.path.join(os.path.dirname(__file__), "links.json"), "r") as file:
+        json_links = json.load(file)[0]
+    
+    links = json_links["links"]
+    users = json_links["user"]
+    github_users = json_links["github_users"]
 
-    return render(request, 'GithubReview/description.html', {"github_kevin" : var, "name_project" : name_project, "github_stefan" : var2, "name_project2" : name_project2, "github_leo" : var3, "name_project3" : name_project3,
-     "github_elias" : var4, "name_project4" : name_project4, "github_alexander" : var5, "name_project5" : name_project5, "github_michael" : var6, "name_project6" : name_project6})
+    menu = projectlist_htmlwrapper(links, users)
+    repos = repoview_htmwrapper(github_users)
+    
+
+
+    return render(request, 'GithubReview/description.html', {"links" : menu, "project_information" : repos})
 
 '''
   - Links in Config-Datei machen (eigene Datei json Datei, die verknüpft werden soll)
@@ -102,15 +114,3 @@ def description(request):
   - Github Token
   - iframe für Infos
 '''
- 
-def get_links():
-    input_file = open('links.json')
-    json_array = json.load(input_file)
-    links_list = []
- 
-    for item in json_array:
-        store_details = {"links":None}
-        store_details['links'] = item['links']
-        links_list.append(store_details)
- 
-    print(links_list)
